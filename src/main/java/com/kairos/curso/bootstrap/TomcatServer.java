23932144fe4e1f204c16d5afb0c3fef8c7fa965d
Edgar G <edgargmaya@gmail.com>
2019-11-22 21:48:34 -0600

    Creacion de clase independiente de Tomcat

---
+++ src/main/java/com/kairos/curso/bootstrap/TomcatServer.java
@@ -0,0 +1,35 @@
+package com.kairos.curso.bootstrap;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import com.kairos.curso.servlet.DemoServlet;
+
+public class TomcatServer {
+	
+	
+	
+	public void start() throws LifecycleException {
+		
+		final Tomcat server = new Tomcat();
+		Context myContext = server.addContext("", null);
+
+		server.setPort(8090);
+
+		Tomcat.addServlet(myContext, "demoServlet", new DemoServlet());
+		myContext.addServletMappingDecoded("/saludo", "demoServlet");
+
+		server.start();
+
+		/*
+		 * COMENTARIO: FORMA TRADICIONAL EN JAVA DE CREAR OTRO HILO DE EJECUCIÓN INDEPENDIENTE AL PRINCIPAL
+		 */
+		/*
+		 * new Thread() { public void run() { server.getServer().await(); } }.run();
+		 */
+
+		new Thread(() -> server.getServer().await()).run();
+	}
+
+}
